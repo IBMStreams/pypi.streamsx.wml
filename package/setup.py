bf943001d9a3cc8734ab7b1db3ad8339ec94fe26
rolefhei <rolefhei@de.ibm.com>
2020-10-20 17:30:22 +0200

    Development status set to 4 (Beta)

--- package/setup.py
+++ package/setup.py
@@ -13,7 +13,7 @@ setup(
   url = 'https://github.com/IBMStreams/pypi.streamsx.wml',
   keywords = ['streams', 'ibmstreams', 'streaming', 'analytics', 'streaming-analytics', 'wml', 'streamsx' , 'topology'],
   classifiers = [
-    'Development Status :: 1 - Planning',
+    'Development Status :: 4 - Beta',
     'License :: OSI Approved :: Apache Software License',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.5',
